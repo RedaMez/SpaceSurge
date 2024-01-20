1165e49b63a39e1f66471804c4ec03ba1b385308
MEZIANE reda <reda.meziane@etu.univ-paris-diderot.fr>
2024-01-20 15:50:46 +0100

    Initial Commit

---
+++ Space Surge/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Space Surge
+//
+//  Created by Reda Meziane on 20/01/2024.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
